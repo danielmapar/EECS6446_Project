5ca9a739cf8ed10897fa280e1dcdc626de21b0fd
Ubuntu <ubuntu@eecs6446proj1-1.arbutus>
2021-04-13 06:41:02 +0000

    Adding port as env var

--- custom-autoscaler/api/app.py
+++ custom-autoscaler/api/app.py
@@ -105,4 +105,4 @@ def get_all_loadtests():
   return jsonify({'result' : output})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('API_PORT')))
